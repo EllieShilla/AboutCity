7568ed7c71fdabd61d991224c3f6439e79b5317f
EllieShilla <nosova.alina97@gmail.com>
2021-05-25 20:48:32 +0300

    25.05
    25.05

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, SWitch, Link} from 'react-router-dom';
+
+function KR(){
+  return <h2></h2>
+}
+function MainShowplace(){
+  return <h2></h2>
+}
+function Showplaceы(){
+  return <h2></h2>
+}
+function Photos(){
+  return <h2></h2>
+}
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
